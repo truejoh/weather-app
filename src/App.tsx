1788cd9777eb32a6786f5983ae0ab350280430a4
truejoh <jonny.zhong.dev@gmail.com>
2021-11-17 07:50:13 -0600

    weather slider

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,47 @@
 import React, { ReactElement, useEffect, useState } from 'react';
+import Slider, { Handle, SliderTooltip } from 'rc-slider';
 
-import { Container } from './styles';
+import { Container, WeatherContainer } from './styles';
 
 import './App.css';
+import 'rc-slider/assets/index.css';
 
 import { getColorByTemp, getWeatherIconUrl } from './utils/helper';
 import { getLocations, getWeather } from './api/apis';
 
 // eslint-disable
 type WeatherType = {
-  id: number;
-  applicable_date: Date;
-  weather_state_name: string;
-  weather_state_abbr: string;
-  wind_speed: number;
-  wind_direction: number;
-  wind_direction_compass: string;
-  min_temp: number;
-  max_temp: number;
-  the_temp: number;
+  id?: number;
+  applicable_date?: Date;
+  weather_state_name?: string;
+  weather_state_abbr?: string;
+  wind_speed?: number;
+  wind_direction?: number;
+  wind_direction_compass?: string;
+  min_temp?: number;
+  max_temp?: number;
+  the_temp?: number;
 };
+
+interface ISliderHandleProps {
+  className: string;
+  prefixCls?: string;
+  vertical?: boolean;
+  offset: number;
+  value: number;
+  dragging?: boolean;
+  disabled?: boolean;
+  min?: number;
+  max?: number;
+  reverse?: boolean;
+  index: number;
+  tabIndex?: number;
+  ariaLabel: string;
+  ariaLabelledBy: string;
+  ariaValueTextFormatter: string;
+  style?: React.CSSProperties;
+  ref?: React.Ref<any>;
+}
 
 function App(): ReactElement {
   const [weather, setWeather] = useState<WeatherType>();
@@ -47,12 +69,42 @@ function App(): ReactElement {
     );
   }, []);
 
+  const handle = (props: ISliderHandleProps): ReactElement => {
+    const { value, dragging, index, ariaValueTextFormatter, ...restProps } =
+      props;
+    return (
+      <SliderTooltip
+        prefixCls="rc-slider-tooltip"
+        overlay={`${value} C`}
+        visible={dragging}
+        placement="top"
+        key={index}
+      >
+        <Handle value={value} {...restProps} />
+      </SliderTooltip>
+    );
+  };
+
+  const handleChange = (value: number): void => {
+    setWeather({ ...weather, the_temp: value });
+  };
+
   return (
     <Container bgColor={getColorByTemp(weather?.the_temp)}>
-      <p>{weather?.the_temp}</p>
-      <img
-        src={getWeatherIconUrl(weather?.weather_state_abbr)}
-        alt="weather-icon"
+      <WeatherContainer>
+        <img
+          src={getWeatherIconUrl(weather?.weather_state_abbr)}
+          alt="weather-icon"
+        />
+        <br />
+        <p>{weather?.the_temp}</p>
+      </WeatherContainer>
+      <Slider
+        className="slider"
+        min={-50}
+        max={50}
+        handle={handle}
+        onChange={handleChange}
       />
     </Container>
   );
--- src/styles.ts
+++ src/styles.ts
@@ -1,10 +1,19 @@
 import styled from 'styled-components';
 
 export const Container = styled.div<{ bgColor: string }>`
+  position: relative;
   width: 100wv;
   height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
   background-color: ${({ bgColor }) => bgColor};
+
+  .slider {
+    position: absolute;
+    bottom: 50px;
+    width: 300px;
+  }
 `;
+
+export const WeatherContainer = styled.div``;
